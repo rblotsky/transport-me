ff79df8de6743e1769d0a9781755dd90e284055e
DanWang725 <dw.smaug@gmail.com>
2025-05-28 18:23:18 -0400

    add new visualization system

---
+++ Main/DevTools/DebugVisualization.cs
@@ -0,0 +1,21 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transportme.Main.DevTools
+{
+    public struct DebugVisualization
+    {
+        public Mesh Mesh;
+        public Vector3 Position;
+        public Quaternion Rotation;
+    }
+
+    public interface IDebugVisualizationProvider
+    {
+        IEnumerable<DebugVisualization> GetVisualization();
+    }
+}
---
+++ Main/DevTools/DebugVisualizationFactory.cs
@@ -0,0 +1,68 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vector3 = Godot.Vector3;
+
+namespace Transportme.Main.DevTools
+{
+    public static class DebugVisualizationFactory
+    {
+        public static DebugVisualization Sphere(Vector3 position, float radius, Color colour, float alpha = 1f)
+        {
+            return new()
+            {
+                Mesh = EasyShapes.SphereMesh(radius, EasyShapes.ColouredMaterial(colour, alpha)),
+                Position = position,
+                Rotation = Quaternion.Identity,
+            };
+        }
+
+        public static DebugVisualization Curve(Vector3 start, Vector3 end, Vector3 control, Color colour, int numSegments = 10)
+        {
+            return new()
+            {
+                Mesh = EasyShapes.CurveMesh(start, end, control, colour, numSegments),
+                Position = start,
+                Rotation = Quaternion.Identity,
+            };
+        }
+
+        public static DebugVisualization Arrow(Vector3 start, Vector3 end, Color colour, float alpha = 1f)
+        {
+            var forward = (end - start).Normalized();
+            return new()
+            {
+                Mesh = EasyShapes.TrianglePointerMesh(colour, alpha),
+                Position = start,
+                Rotation = new Basis(Vector3.Forward, forward, Vector3.Right).GetRotationQuaternion(),
+            };
+        }
+
+        public static DebugVisualization Line(Vector3 start, Vector3 end, Color colour, float alpha = 1f)
+        {
+            return new()
+            {
+                Mesh = EasyShapes.LineMesh(start, end, colour),
+                Position = start,
+                Rotation = Quaternion.Identity,
+            };
+        }
+
+        public static DebugVisualization Box(Vector3 position, Quaternion rotation, Vector3? size, Color colour, float alpha = 1f)
+        {
+            return new()
+            {
+                Mesh = new BoxMesh
+                {
+                    Size = size ?? new Vector3(1, 1, 1),
+                    Material = EasyShapes.ColouredMaterial(colour, alpha),
+                },
+                Position = position,
+                Rotation = rotation,
+            };
+        }
+    }
+}
